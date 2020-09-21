Corrected exact, monopolar jet flux calculation method from Equation 8 of Reynolds (1986), physics.flux_expected_r86. Added notes folder to contain mathematical background of functions used. Various corrections to plotting within the Pipeline class.

files/example-model-params.py
@@ -10,26 +10,26 @@ import numpy as np
 import scipy.constants as con
 
 params = {
-    "target": {"name": "S255IRS3",  # Jet/YSO/... name
-               "ra": "06:12:54.02",  # HH:MM:SS.SS... [J2000]
-               "dec": "+17:59:23.6",  # DD:MM:SS.SS... [J2000]
+    "target": {"name": "M-FH",  # Jet/YSO/... name
+               "ra": "06:00:00.00",  # HH:MM:SS.SS... [J2000]
+               "dec": "+18:00:00.0",  # DD:MM:SS.SS... [J2000]
                "epoch": "J2000",
-               "dist": 1780.,  # pc
-               "v_lsr": 7.4,  # km/s
-               "m_star": 10.0,  # M_sol
-               "r_1": 1.0,  # inner disc radii sourcing the jet in au
-               "r_2": 5.0,  # outer disc radii sourcing the jet in au
+               "dist": 700.,  # pc
+               "v_lsr": 0.0,  # km/s
+               "m_star": .5,  # M_sol
+               "r_1": 0.06,  # inner disc radii sourcing the jet in au
+               "r_2": 0.06,  # outer disc radii sourcing the jet in au
                },
-    "grid": {"n_x": 40,  # No. of cells in x
-             "n_y": 40,  # No. of cells in y
+    "grid": {"n_x": 50,  # No. of cells in x
+             "n_y": 50,  # No. of cells in y
              "n_z": 100,  # No. of cells in z
-             "l_z": 0.5,  # Length of z-axis in arcsec. Overrides n_x/n_y/n_z.
-             "c_size": 1.0,  # Cell size (au)
+             "l_z": .1,  # Length of z-axis in arcsec. Overrides n_x/n_y/n_z.
+             "c_size": 0.2,  # Cell size (au)
              },
     "geometry": {"epsilon": 9. / 9.,  # Jet width index
-                 "opang": 30.,  # Jet opening angle (deg)
-                 "w_0": 5.0,  # Half-width of jet base (au)
-                 "r_0": 1.0,  # Launching radius (au)
+                 "opang": 20.,  # Jet opening angle (deg)
+                 "w_0": 0.06,  # Half-width of jet base (au)
+                 "r_0": 0.00,  # Launching radius (au)
                  "inc": 90.,  # Inclination angle (deg)
                  "pa": 0.,  # Jet position PA (deg)
                  "exp_cs": False,  # Transverse exp. density profile?
@@ -38,16 +38,16 @@ params = {
                    "q_T": 0.,  # Temperature index
                    "q_x": 0.,  # HII fraction index
                    },
-    "properties": {"v_0": 500.,  # Ejection velocity (km/s)
+    "properties": {"v_0": 250.,  # Ejection velocity (km/s)
                    "x_0": 0.1,  # Initial HII fraction
-                   "n_0": 2.6e9,  # Initial density (cm^-3)
+                   "n_0": None,  # Initial density (cm^-3)
                    "T_0": 1E4,  # Temperature (K)
                    "mu": 1.3,  # Mean atomic weight (m_H)
-                   "mlr": 1e-6,  # Msol / yr
+                   "mlr": 1e-8,  # Msol / yr
                    },
-    "ejection": {"t_0": np.array([]),  # Peak times of bursts (yr)
-                 "hl": np.array([]),  # Half-lives of bursts (yr)
-                 "chi": np.array([]),  # Burst factors
+    "ejection": {"t_0": np.array([1200.]),  # Peak times of bursts (yr)
+                 "hl": np.array([0.5]),  # Half-lives of bursts (yr)
+                 "chi": np.array([2.]),  # Burst factors
                  }
              }
 

maths/physics.py
@@ -193,9 +193,7 @@ def import_vanHoof2014(errors=False):
     if errors:
         from uncertainties import ufloat as uf
 
-    datafile = os.sep.join([os.path.expanduser('~'), "Dropbox",
-                            "SpyderProjects", "vajepy", "files",
-                            "vanHoofetal2014.data"]) 
+    datafile = os.sep.join([cfg.dcys['files'], "vanHoofetal2014.data"])
 
     data = []
     with open(datafile, 'rt') as f:
@@ -214,8 +212,8 @@ def import_vanHoof2014(errors=False):
         unc_lines = [[float(_) for _ in l.split()] for l in lines[192:]]
         
         logus = np.linspace(np.round(logu_start, decimals=1),
-                            np.round(logu_start + (step * (n_logu - 1)), decimals=1),
-                            n_logu)
+                            np.round(logu_start + (step * (n_logu - 1)),
+                                     decimals=1), n_logu)
 
         loggam2s = np.linspace(np.round(loggam2_start, decimals=1),
                                np.round(loggam2_start +